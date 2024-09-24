cd61469c160242f7a3459d14a433d8aa0c5fae21
Paloma Cardozo <pmc8@gatech.edu>
2024-09-24 16:38:13 -0400

    Updated streaming and Kernel etc

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import cv2
 import numpy as np
 import subprocess
 import datetime
+import yt_dlp
 
-stream_url = "https://www.youtube.com/watch?v=OlwC1weFdAg"
+# Function to get the direct stream URL using yt-dlp
+def get_stream_url(youtube_url):
+    ydl_opts = {
+        'format': 'best',  # Select the best available format
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(youtube_url, download=False)
+        video_url = info_dict.get("url", None)
+    return video_url
+
+stream_url = get_stream_url("https://www.youtube.com/watch?v=OlwC1weFdAg")
 cap = cv2.VideoCapture(stream_url)
+motion_threshold = 100
 
 if not cap.isOpened():
     print("Error: Couldn't open the bee honey project stream.")
@@ -36,10 +48,15 @@ while True:
         continue
     # Calculate the difference between the current frame and the previous frame
     frame_delta = cv2.absdiff(previous_frame, gray)
+
+    # Apply a threshold to the difference to create a binary image
     thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
 
-    # Dilate the threshold image to fill in holes, then find contours
-    thresh = cv2.dilate(thresh, None, iterations=2)
+    # Dilate the thresholded image to fill in small holes
+    kernel = np.ones((5, 5), np.uint8)  # Create a kernel for dilation
+    thresh = cv2.dilate(thresh, kernel, iterations=2)
+
+    #Find contours of the thresholded image
     contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Check if significant motion is detected based on contour areas
